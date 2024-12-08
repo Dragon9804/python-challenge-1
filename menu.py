b2268bdfb979a8e0a5c5bfc1b56361689911824b
Dragon9804 <jason.smoody@yahoo.com>
2024-12-08 11:18:39 -0700

    more sum adjustments

--- menu.py
+++ menu.py
@@ -242,19 +242,18 @@ for menu_item in order_list:
 # 11. Calculate the cost of the order using list comprehension
 # Multiply the price by quantity for each item in the order list, then sum()
 # and print the prices.
-    #r = 0
-    #for menu_item in order_list:
-        #order_list[r] = {
-            #"Item name": menu_item["Item name"],
-            #"Price": menu_item["Price"],
-            #"Quantity": menu_item["Quantity"],
-            #"Total": menu_item["Price"] * menu_item["Quantity"]
-        #}
-    #r += 1
+    r = 0
+    for menu_item in order_list:
+        order_list[r] = {
+            "Item name": menu_item["Item name"],
+            "Price": menu_item["Price"],
+            "Quantity": menu_item["Quantity"],
+            "Total": menu_item["Price"] * menu_item["Quantity"]
+        }
+    r += 1
 
-    #total_cost = sum([menu_item_name["Total"] for menu_item_name in order_list])
+    total_cost = sum([menu_item_name["Total"] for menu_item_name in order_list])
     
     #total_cost = sum([menu_item_name["Price"] * menu_item_name["Quantity"] for menu_item_name in order_list])
 
-total_cost = sum([menu_item_name["Total"]])
 print(f"Total cost: ${total_cost:.2f}")
